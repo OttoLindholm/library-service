2f32a770020a9f642d25fa5dc5401f4dd3889da2
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2025-01-16 17:38:58 +0200

    create borrowing view set

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+
+class BorrowingViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = BorrowingSerializer
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer
