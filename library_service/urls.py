65545a521de088a686b768fc7982fdd05e335b1d
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2025-01-16 17:40:55 +0200

    add routers for borrowing

--- library_service/urls.py
+++ library_service/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/book/", include("book.urls", namespace="book")),
     path("api/v1/user/", include("user.urls", namespace="user")),
+    path(
+        "api/v1/borrowing/", include("borrowing.urls", namespace="borrowing")
+    ),
 ]
