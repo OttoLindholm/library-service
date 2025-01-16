5b6117cfc9c3dd87c652323b7ac18f324175f726
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2025-01-16 17:25:37 +0200

    add borrowing serializer with book detail

---
+++ borrowing/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from borrowing.models import Borrowing
+from book.models import Book
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all())
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        ]
