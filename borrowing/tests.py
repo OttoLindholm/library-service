6cdac8789ced543bf3a4cd78452df134c82a712f
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2025-01-16 21:12:05 +0200

    fix url in book tests, add test for unauthenticated users for borrowing

--- borrowing/tests.py
+++ borrowing/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from http.client import responses
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+
+BORROWING_LIST_URL = reverse("borrowings:borrowing-list")
+
+
+class UnauthenticatedTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_unauthenticated_users_cannot_access(self):
+        response = self.client.get(BORROWING_LIST_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
