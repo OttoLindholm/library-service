2c7f984321a8834dd4d828b8d3e620bfa41dbaad
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2025-01-16 15:57:53 +0200

    add test for authenticated users

--- book/tests.py
+++ book/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 from book.models import Book
 
@@ -46,3 +47,45 @@ class UnauthenticatedTests(TestCase):
     def test_cannot_delete_book(self):
         response = self.client.delete(self.book_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedUserTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="tes@user.com", password="password"
+        )
+        self.client.force_authenticate(user=self.user)
+
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Test Author",
+            cover="SOFT",
+            inventory=5,
+            daily_fee=1,
+        )
+        self.book_url = f"{BOOK_LIST_URL}{self.book.id}/"
+
+    def test_authenticated_user_can_access_book_list(self):
+        response = self.client.get(BOOK_LIST_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_authenticated_user_cannot_create_book(self):
+        data = {
+            "title": "Title",
+            "author": "Test Author",
+            "cover": "SOFT",
+            "inventory": 1,
+            "daily_fee": 1,
+        }
+        response = self.client.post(BOOK_LIST_URL, data=data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_authenticated_user_cannot_update_book(self):
+        data = {"title": "Updated Title"}
+        response = self.client.patch(self.book_url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_authenticated_user_cannot_delete_book(self):
+        response = self.client.delete(self.book_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
